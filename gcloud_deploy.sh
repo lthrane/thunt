aefcf0f141a789d13a8c562c80a4a37e03d0db6b
Leon Thrane <leon@thrane.net>
2020-02-22 12:48:20 -0800

    updated deployment script to use modern GCE methods

--- gcloud_deploy.sh
+++ gcloud_deploy.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-gcloud compute instances create thunt \
-    --image cos-stable-80-12739-68-0 \
-    --image-project cos-cloud \
-    --metadata-from-file google-container-manifest=containers.yaml \
-    --tags http-server \
-    --zone us-central1-a \
-    --machine-type f1-micro
+gcloud compute instances create-with-container thunt \
+    --container-image=registry.hub.docker.com/lthrane/thunt
